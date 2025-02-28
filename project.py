d19ff4d32c86c65c3e3681ce72aec82d6d8c2308
Mohsin Rafeeq <85779322+MohsinRafeeq@users.noreply.github.com>
2025-02-28 23:26:09 +0500

    Add files via upload
    The **Band Name Generator** project is a fun Python application that randomly combines adjectives and nouns to generate unique and creative band names. Using Python's `random` module, the program selects a random word from two predefined lists: one for adjectives and one for nouns. The result is a band name like "The Electric Phoenix" or "The Funky Vibes." This simple yet entertaining project allows for easy customization by adding more words to the lists, and it serves as a great introduction to Python fundamentals like lists, functions, and random selections. It's perfect for practicing basic Python skills while creating a playful and engaging tool.

---
+++ project.py
@@ -0,0 +1,7 @@
+print("welcome to the band name generator !")
+
+street = input("what's name of the city you grew up in ?\n")
+
+pet = input("what's your pet name ?\n")
+
+print("THE BAND NAME IS " + street+"_"+pet)
